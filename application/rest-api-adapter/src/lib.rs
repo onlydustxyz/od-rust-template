b4e197715f8060c4ce6bcdf9e7e59ebe910ade0b
PierreOucif <pierre.oucif@gadz.org>
2023-07-06 17:57:03 +0200

    Init commit with a running http server

---
+++ application/rest-api-adapter/src/lib.rs
@@ -0,0 +1,13 @@
+#[macro_use]
+pub extern crate rocket;
+use rocket::{Build, get, Rocket};
+
+#[get("/")]
+pub fn index() -> &'static str {
+    "Hello, world!"
+}
+
+
+pub fn build_api() -> Rocket<Build> {
+    rocket::build().mount("/", routes![index])
+}
---
+++ bootstrap/src/main.rs
@@ -0,0 +1,12 @@
+extern crate rocket;
+
+use rest_api_adapter::build_api;
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    build_api()
+        .launch()
+        .await?;
+    Ok(())
+}
+
